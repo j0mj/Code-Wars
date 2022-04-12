ac514743cddc3ace97cc8210361922d649e6571c
j0mj <jomj15@gmail.com>
2022-04-11 22:21:09 -0500

    new

---
+++ type_casting.js
@@ -0,0 +1,5 @@
+function numberToString(num) {
+    return num.toString();
+  }
+  
+  //had to look up what type casting was, which is essentially a built-in method to change data types to and fro.
